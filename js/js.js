161dc8dabf4ee158eb8be723362d98a27c14ce1e
Veldriant <65666734+Veldriant@users.noreply.github.com>
2020-12-01 00:05:21 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+$(function() {
+  document.getElementById('feedback-form').addEventListener('submit', function(evt){
+    var http = new XMLHttpRequest(), f = this;
+    var th = $(this);
+    evt.preventDefault();
+    http.open("POST", "mail.php", true);
+    http.onreadystatechange = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+        if (http.responseText.indexOf(f.nameFF.value) == 0)
+          th.trigger("reset");
+        }
+      }
+    }
+    http.onerror = function() {
+      alert('Извините, данные не были переданы');
+    }
+    http.send(new FormData(f));
+  }, false);
+
+});
